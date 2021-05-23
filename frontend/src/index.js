fba4f490434168020a4465c6e7865d81d1aea00c
Tomasz Kukla <79609921+tomasz-kukla@users.noreply.github.com>
2021-05-23 13:52:51 +0200

    Init react app

---
+++ frontend/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <h1>Dupa</h1>/>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
+reportWebVitals();
