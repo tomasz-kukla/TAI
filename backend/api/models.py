300ff147ec669a6735a392f1da4992efe941f293
Tomasz Kukla <79609921+tomasz-kukla@users.noreply.github.com>
2021-05-23 13:14:08 +0200

    Create db models

---
+++ backend/api/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.db.models.deletion import CASCADE
+
+
+class Brand(models.Model):
+    name = models.CharField(max_length=30)
+    category = models.CharField(max_length=20)
+    origin = models.CharField(max_length=20)
+    established = models.DateField()
+
+    def __str__(self) -> str:
+        return f'{self.name}'
+
+
+class Shop(models.Model):
+    brand = models.ForeignKey(Brand, related_name='shops', on_delete=CASCADE)
+
+    city = models.CharField(max_length=30)
+    mall_name = models.CharField(max_length=40)
+    customers = models.IntegerField()
+    opened = models.DateField()
+
+    def __str__(self) -> str:
+        return f'{self.city} - {self.mall_name} - {self.opened}'
