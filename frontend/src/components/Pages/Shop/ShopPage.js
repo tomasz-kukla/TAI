b401df8ab1207054754c90c9f9e3d4d1cf6d6e5d
Tomasz Kukla <79609921+tomasz-kukla@users.noreply.github.com>
2021-06-01 02:10:01 +0200

    Create Shop View

--- frontend/src/components/Pages/Shop/ShopPage.js
+++ frontend/src/components/Pages/Shop/ShopPage.js
@@ -1,12 +1,35 @@
-import React, { Component } from 'react';
-import { useState } from "react";
-import { render } from "react-dom";
+import { useState, useEffect } from 'react';
+
+import { Container } from 'react-bootstrap';
+
+import axios from 'axios';
 
 export const ShopPage = (props) => {
+    const [shops, setShops] = useState([]);
+
+    useEffect(() => {
+        axios.get("http://127.0.0.1:8000/api/Shops/")
+            .then(res => {
+                setShops(res.data)
+            })
+            .catch(console.log)
+    }, []);
+
+
 
     return (
         <div>
-            <h1>Shop Page</h1>
+            <h2 >Shops</h2>
+            {shops.map((shop) => (
+                <Container>
+                    <div>
+                        <ul class="list-group">
+                            <li class="list-group-item "  key={shop.id}>{shop.city} - {shop.estabished}</li>
+                        </ul>
+                    </div>
+                </Container>
+
+            ))}
         </div>
     );
 
