185b6c7c08bfed92ce0cf53c9b1a030d5ed09c44
Tomasz Kukla <79609921+tomasz-kukla@users.noreply.github.com>
2021-06-01 02:09:34 +0200

    Create all brands view

--- frontend/src/components/Pages/Brand/BrandPage.js
+++ frontend/src/components/Pages/Brand/BrandPage.js
@@ -1,13 +1,67 @@
-import React, { Component } from 'react';
-import { useState } from "react";
-import { render } from "react-dom";
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+import { Container, Button } from 'react-bootstrap';
+
+import styled from 'styled-components';
+
+import { BrandDetail } from './BrandDetail';
+
 
 export const BrandPage = (props) => {
+    const [brands, setBrands] = useState([]);
+
+    useEffect(() => {
+        axios.get("http://127.0.0.1:8000/api/Brands/")
+            .then(res => {
+                setBrands(res.data)
+            })
+            .catch(console.log)
+    }, []);
+
+    const handleAdd = () => {
+        console.log("Add")
+    }
+
 
     return (
-        <div>
-            <h1>Brand Page</h1>
-        </div>
+        <div className="container text-center">
+            <h2>Brands</h2>
+            <a class="btn btn-secondary float-right" onClick={handleAdd}>Create Brand</a>
+
+            {brands.map((brand) => (
+                <Div className="card text-center" styles={`width: 20rem`}>
+                    <div className="card-header">
+                        <h5 className="card-title" key={brand.id}>
+                            {brand.name}</h5>
+                    </div>
+
+                    <div className="card-body">
+                        <t className="card-text" key={brand.id}>
+                            Established: {brand.established}</t>
+                        <br />
+                        <t className="card-text" key={brand.id}>
+                            Country: {brand.origin}</t>
+                    </div>
+
+                    <div className="card-footer">
+                        <a
+                            onClick={() => BrandDetail(brand)}
+                            href={'/Brands/' + brand.id + "/"}
+                            class="btn btn-danger"
+                        >
+                            Edit
+                        </a>
+                    </div>
+                </Div>
+
+            ))
+            }
+        </div >
     );
 
 };
+
+const Div = styled.div`
+    margin-bottom: 30px;
+    
+`
